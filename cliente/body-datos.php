ee5cb8b5629ea29da21b36a65836694301d7b8f8
SDWarkXD <sdwark0925@gmail.com>
2021-10-11 07:27:07 -0500

    Mis datos terminado

--- cliente/body-datos.php
+++ cliente/body-datos.php
@@ -14,27 +14,44 @@
 
         </div>
 
-        <div class="d-flex  flex-column  align-items-center justify-content-around clientes-registrados-container">
+        <div class="d-flex  flex-column  align-items-center justify-content-around clientes-registrados-container">            
+            <?php
+                $id=intval($_SESSION['id']);
+                $link=mysqli_connect("localhost","root","");
+                mysqli_select_db($link,"mercurioDB");
+                $result=mysqli_query($link,"select * from Users where id_user=$id");
+                while($row=mysqli_fetch_array($result)){
+                    $correo=$row['correo'];
+                    $pass=$row['pass'];
+                    $nombre=$row['nombre'];
+                    $tel=$row['telefono'];
 
-            <div class="clientes-registrados card-borde">
-                <?php
-                    $id=intval($_SESSION['id']);
-                    $link=mysqli_connect("localhost","root","");
-                    mysqli_select_db($link,"mercurioDB");
-                    $result=mysqli_query($link,"select * from Users where id_user=$id");
-                    while($row=mysqli_fetch_array($result)){
-                        $correo=$row['correo'];
-                        $pass=$row['pass'];
-                        $nombre=$row['nombre'];
-                        $tel=$row['telefono'];
-                        echo $id;
-                    }
-                ?>
-            </div>
+                    echo "<div class='clientes-registrados card-borde'>";
+                    echo "<p class='datos-cuenta-info-titulo'> datos cuentas </p>";
+                    echo "<div class='row datos-cuenta-info'>"; 
+                    echo "<div class='col'>"; 
+                    echo "<p class='datos-cuenta-info-label '> Correo electrónico </p>";
+                    echo "<p class='datos-cuenta-info-label'> Contraseña </p>";
+                    echo "</div> <div class='col'>";                     
+                    echo "<p class='datos-cuenta-info-texto'> $correo </p>";
+                    echo "<p type'password' class='datos-cuenta-info-texto'> ************ </p>";
+                    echo "</div></div></div>";
 
-            <div class="mis-datos-area-doble card-borde">
-            </div>
+                    
 
+                    echo "<div class='clientes-registrados card-borde'>";
+                    echo "<p class='datos-cuenta-info-titulo'> datos personales </p>";
+                    echo "<div class='row datos-cuenta-info'>"; 
+                    echo "<div class='col'>"; 
+                    echo "<p class='datos-cuenta-info-label '> Nombre </p>";
+                    echo "<p class='datos-cuenta-info-label'> Teléfono </p>";
+                    echo "</div> <div class='col'>";                     
+                    echo "<p class='datos-cuenta-info-texto'> $nombre </p>";
+                    echo "<p type'password' class='datos-cuenta-info-texto'> $tel </p>";
+                    echo "</div></div></div>";
+                    
+                }
+            ?>
         </div>
 
     </main>
