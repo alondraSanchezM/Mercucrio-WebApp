64a2f52872679c68f6c9328c5a1b0794f495d196
alo_sanchez <alondra1d35@gmail.com>
2021-10-15 16:11:55 -0500

    :sparkles: Show administrator menus

--- administrador/body-clientes.php
+++ administrador/body-clientes.php
@@ -15,16 +15,44 @@
 
         </div>
 
-        <div class="d-flex  flex-column  align-items-center justify-content-around clientes-registrados-container">
+        <?php
+            $link=mysqli_connect("localhost","root","");
+            mysqli_select_db($link,"mercurioDB");
+            $link->set_charset("utf8");
+            $result=mysqli_query($link,"select * from users where tipo=1");
+            echo "<div class='d-flex  flex-column  align-items-center justify-content-around clientes-registrados-container'>";
+                while($row=mysqli_fetch_array($result)){
+                    echo "<div class='clientes-registrados card-borde'>";
+                    $id_u=$row['id_user'];
+                    $nom=$row['nombre'];
+                    $corr=$row['correo'];
+                    $fecha=$row['fecha_de_registro'];
 
-        <div class="clientes-registrados card-borde">
-            </div>
+                    //Total de productos registrados
+                    $res = mysqli_query($link,"SELECT COUNT(*) total FROM Productos WHERE id_user=$id_u");
+                    $fila = mysqli_fetch_assoc($res);
+                    $totProd =  $fila['total'];
 
-            <div class="clientes-registrados card-borde">
-            </div>
-
-            <div class="clientes-registrados card-borde">
-            </div>
+                    //Total de intercambios realizados
+                    $res = mysqli_query($link,"SELECT COUNT(*) total FROM Intercambios WHERE id_solicitante =$id_u"); //como solicitante
+                    $fila = mysqli_fetch_assoc($res);
+                    $totSoli =  $fila['total'];
+                    $res = mysqli_query($link,"SELECT COUNT(*) total FROM Intercambios WHERE id_solicitado =$id_u"); //como solicitado
+                    $fila = mysqli_fetch_assoc($res);
+                    $totSoli1 =  $fila['total'];
+                    $totInter = $totSoli + $totSoli1;
+                    
+                    echo "<div class='d-flex  flex-row align-self-start'> <img  class='card-mis-productos-imagen' src='../images/user.svg' >";
+                    echo "<div class='d-flex  flex-column card-mis-productos-texto align-self-start'>
+                            <p class='card-mis-productos-titulo'>$nom</p> <p class='card-mis-productos-titulo'>#Id. $id_u</p> <p class='card-mis-productos-titulo'>$corr</p> </div> ";
+                    echo "<div class='d-flex  flex-column card-mis-productos-datos-finales col align-self-end' > ";                  
+                    echo "<p  class='card-mis-productos-fecha col align-self-end'> TOTAL DE PRODUCTOS REGISTRADOS: $totProd</p> 
+                        <p  class='card-mis-productos-fecha col align-self-end'> TOTAL DE INTERCAMBIOS REALIZADOS: $totInter</p> 
+                        <p  class='card-mis-productos-fecha col align-self-end'> Fecha de registro: $fecha</p> </div></div></div> ";
+                    //echo "<div class='d-flex  flex-row align-self-start'> <img  class='card-intercambios-imagen' src='../images/trash.svg'>";
+                }
+            echo "</div>";
+        ?>
 
         </div>
 
--- administrador/body-productos.php
+++ administrador/body-productos.php
@@ -5,7 +5,6 @@
     echo "<body>";
     require_once 'header-administrador.php';
 ?>
-
     <main class="principal">
 
         <div class="d-flex align-items-center justify-content-around">
@@ -15,26 +14,38 @@
 
         </div>
 
-        <div class="d-flex  flex-column  align-items-center justify-content-around clientes-registrados-container">
+        <?php
+            $link=mysqli_connect("localhost","root","");
+            mysqli_select_db($link,"mercurioDB");
+            $link->set_charset("utf8");
+            $result=mysqli_query($link,"select * from productos");
+            echo "<div class='d-flex  flex-column  align-items-center justify-content-around clientes-registrados-container'>";
+                while($row=mysqli_fetch_array($result)){
+                    echo "<div class='clientes-registrados card-borde'>";
+                    $id_p=$row['id_producto'];
+                    $id_u=$row['id_user'];
+                    $cat=$row['categoria'];
+                    $nom=$row['nombre'];
+                    $est=$row['estado'];
+                    $mun=$row['municipio'];
+                    $fecha=$row['fecha'];
+                    $ima=$id_u.$id_p.'.jpg';
+                    $desp=$row['descripcion'];
 
-            <div class="clientes-registrados card-borde">
-            </div>
-
-            <div class="clientes-registrados card-borde">
-            </div>
-
-            <div class="clientes-registrados card-borde">
-            </div>
-
-        </div>
+                    echo "<div class='d-flex  flex-row align-self-start'> <img  class='card-mis-productos-imagen' src='../images/productos/$ima' >";
+                    echo "<div class='d-flex  flex-column card-mis-productos-texto align-self-start'> <p class='card-mis-productos-titulo'>$nom</p>  <p class='card-mis-productos-descripcion'>$desp</p> </div> ";
+                    echo "<div class='d-flex  flex-column card-mis-productos-datos-finales col align-self-end' > <p class='card-mis-productos-categoria'>$cat</p> ";
+                    echo "<div  class='d-flex  flex-row align-self-end'><img class='card-mis-productos-ubicacion' src='".$subCarp."images/ubicacion.svg'> <p  class='card-mis-productos-ubicacion'>$mun, $est</p></div> ";                    
+                    echo "<p  class='card-mis-productos-fecha col align-self-end'> Publicado: $fecha</p> </div></div></div> ";
+                    //echo "<div class='d-flex  flex-row align-self-start'> <img  class='card-intercambios-imagen' src='../images/trash.svg'>";
+                }
+            echo "</div>";
+        ?>
 
     </main>
-
 <?php          
     require_once '../footer.php';
 ?>
 
-
 </body>
-
 </html>
