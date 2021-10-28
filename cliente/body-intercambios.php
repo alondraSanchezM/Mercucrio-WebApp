5f10852c6eb7ea5b79aecfbb8a3181dcd00fef6a
SDWarkXD <sdwark0925@gmail.com>
2021-10-28 17:55:46 -0500

    modal intercambios

--- cliente/body-intercambios.php
+++ cliente/body-intercambios.php
@@ -20,6 +20,7 @@
             $id=intval($_SESSION['id']);
             echo "<div class='d-flex  flex-column  align-items-center justify-content-around clientes-registrados-container'>";
             $result=mysqli_query($link,"select id_solicitud from intercambios");
+            $counts=0;
             while($row=mysqli_fetch_array($result)){//Recorre todos los intercambios
                 $solicitudes=mysqli_query($link, "select * from solicitudes where id_solicitud=$row[id_solicitud]");//Recupera la solicitud
                 $solicitudes=mysqli_fetch_array($solicitudes);
@@ -43,9 +44,11 @@
                         $imagen=mysqli_fetch_array($imagen);
                         $ima_solicitado=$imagen['nombre'];
                         $desp_solicitado=$producto_solicitado['descripcion'];
+                        
+                        $id_solicitante=$producto_solicitante['id_producto'];
                         echo "<div class='d-flex  flex-row align-self-start'> <img  class='card-intercambios-imagen' src='../images/productos/$ima_solicitado' >";
                         echo "<div class='d-flex  flex-column card-intercambios-texto justify-self-start'> <p class='card-intercambios-titulo'>$nom_solicitado</p>  <p class='card-intercambios-descripcion'>$desp_solicitado</p> </div></div> ";
-                        echo "<div class='card-intercambios-espacio d-flex  flex-row align-self-center'> <button class='card-intercambios-boton card-borde' name='enviar' > Información del intercambio </button>";
+                        echo "<div class='card-intercambios-espacio d-flex  flex-row align-self-center'> <button type='button'  class='card-intercambios-boton card-borde' data-bs-toggle='modal' data-bs-target='#p$counts$id_solicitante'> Información del intercambio </button>";
                         echo "</div>";
                         //Datos producto solicitante
                         $id_solicitante=$producto_solicitante['id_producto'];
@@ -67,7 +70,36 @@
                         $nombre=$user['nombre'];
                         $numero=$user['telefono'];
                         $correo=$user['correo'];
+                        echo "
+                        <div class='modal fade' id='p$counts$id_solicitante' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+                            <div class='modal-dialog modal-lg'>
+                                <div class='modal-content'>
+                                    <div class='modal-header'>
+                                    <h5 class='modal-intercambios-titulo' id='exampleModalLabel'>Información del trueque</h5>
+                                    <button type='button' class='btn-close datos-cuenta-info-texto' data-bs-dismiss='modal' aria-label='Close'></button>
+                                    </div>
+                                    <div class='modal-body'>
+                                    <div class='row'>
+                                        <div class='col-6'>
+                                            <p class='modal-intercambios-info-titulo'>Ubicación</p>
+                                            <p class='modal-intercambios-info-text'>$calle_y_num </p>
+                                            <p class='modal-intercambios-info-text'>$municipio, $estado </p>
+                                            <p class='modal-intercambios-info-text'>$referencia </p>
+                                        </div>
+                                        <div class='col-6'>
+                                        <p class='modal-intercambios-info-titulo'>Datos de contacto</p>
+                                        <p class='modal-intercambios-info-text'>$nombre </p>
+                                        <p class='modal-intercambios-info-text'>$numero </p>
+                                        <p class='modal-intercambios-info-text'>$correo </p>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                            ";
                     }
+                    $counts++;
             }
             echo "</div>";
         ?>
