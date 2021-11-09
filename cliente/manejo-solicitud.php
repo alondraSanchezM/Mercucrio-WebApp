bae246ef518a18385c08e18a7f6fb6621dbf7efe
deisyDanielaG <dedagoto@gmail.com>
2021-11-09 17:36:58 -0600

    Aceptar solicitud

--- cliente/manejo-solicitud.php
+++ cliente/manejo-solicitud.php
@@ -21,18 +21,19 @@
         $id_solicitado=$row['id_user'];
         mysqli_query($link,"update productos set status=1 where id_producto=$producto_solicitado");
         mysqli_query($link,"update solicitudes set status=2 where producto_solicitado=$producto_solicitado");
-        mysqli_query($link,"delete from solicitudes where producto_solicitante=$producto_solicitado");
+        mysqli_query($link,"update solicitudes set status=1 where id_solicitud=$id_sol");
+        mysqli_query($link,"delete from solicitudes where producto_solicitante=$producto_solicitado and status=0");
         //producto solicitante
         $solicitante=mysqli_query($link,"select id_user from productos where id_producto=$producto_solicitante");
-        $row=mysqli_fetch_array($solicitante);
-        $id_solicitante=$row['id_user'];
+        $row2=mysqli_fetch_array($solicitante);
+        $id_solicitante=$row2['id_user'];
         mysqli_query($link,"update productos set status=1 where id_producto=$producto_solicitante");
         mysqli_query($link,"update solicitudes set status=2 where producto_solicitado=$producto_solicitante");
-        mysqli_query($link,"delete from solicitudes where producto_solicitante=$producto_solicitante");
+        mysqli_query($link,"update solicitudes set status=1 where id_solicitud=$id_sol");
+        mysqli_query($link,"delete from solicitudes where producto_solicitante=$producto_solicitante and status=0");
         
         $fecha=date("Y-m-d");
         mysqli_query($link,"insert into Intercambios(id_solicitud,id_solicitado,id_solicitante,fecha) values ($id_sol,$id_solicitado,$id_solicitante,'$fecha')");        
-        mysqli_query($link,"update solicitudes set status=1 where id_solicitud=$id_sol");
 
         header("Location:body-intercambios.php");
     }else if(isset($_GET['declina'])){
